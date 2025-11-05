a22eebe7381e3bb4940c9f641d9a16b32f15b7b8
Moukthik anand <anandmoukthik@gmail.com>
2025-11-05 06:50:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4614812888-a9f78",
+  "appId": "1:335865526095:web:da7e2211472ebddb08b73a",
+  "apiKey": "AIzaSyCEtsLA8IOFIgw-4n9klvhlMOKBc_jXfso",
+  "authDomain": "studio-4614812888-a9f78.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "335865526095"
+};
